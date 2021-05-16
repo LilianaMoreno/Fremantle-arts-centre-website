c50f135970fa27f7e55c5678690269ddf1d9b349
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-05-16 15:27:43 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!--      FOOTER-->
+      <footer class="container-fluid footer">
+          <div class="container">
+              <div class="row">
+              <div class="col-sm-5">
+                  <p class="footer-heading"> CONTACT US </p>
+                  <p class="footer-body">+61 8 9432 9555</p>
+                  <p class="footer-body"> artscentre@fremantle.wa.gov.au</p>
+                  <p class="copyright footer-body">Copyright © 2021 All rights reserved.</p>
+                  </div>
+                    <div class="col-sm-2">
+                        <p class="footer-heading"> LOCATION</p>
+                        <p class="footer-body">1 Finnerty Street Fremantle Western Australia</p>
+                  </div>
+                    <div class="col-sm-3">
+                          <p class="footer-heading"> OPENING TIMES</p>
+                        <p class="footer-body">7 days 10am — 5pm <br>Free admission</p>
+                  </div>
+                    <div class="col-sm-2">
+                        <p class="footer-heading">STAY UPDATED</p>
+                       <a class="footer-body social-media" href="https://www.instagram.com/fremantleartscentre/"> <span class="fab fa-instagram"></span> <span class="social-media-labels"> Instagram </span> </a>
+                        <a class="footer-body social-media" href="https://www.facebook.com/fremantleartscentre"> <span class="fab fa-facebook-square"></span> <span class="social-media-labels"> Facebook</span></a>
+                        <a class="footer-body social-media" href="https://twitter.com/FreoArtsCentre"> <span class="fab fa-twitter"></span> <span class="social-media-labels"> Twitter </span> </a> 
+              <div class="social-media-mobile-grid social-media-mobile">
+                      <a class="social-media-icons" href="https://www.instagram.com/fremantleartscentre/"> <span class="a-labels"> instagram</span> <span class="fab fa-instagram instagram-mobile"></span> </a>  
+                     <a class="social-media-icons" href="https://www.facebook.com/fremantleartscentre"> <span class="a-labels"> facebook</span> <span class="fab fa-facebook-square facebook-mobile"></span> </a>   
+                          <a class="social-media-icons" href="https://twitter.com/FreoArtsCentre"> <span class="a-labels"> twitter</span> <span class="fab fa-twitter twitter-mobile"></span> </a>
+                            </div>
+                  </div>
+              </div>
+          </div>
+      </footer>
+                  
+
+  </body>
+
+    
+    
+    
+</html>
+<?php wp_footer(); ?>
+
