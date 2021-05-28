8266267705f65e7bd45ce1d8b19c0c016d2749ba
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-05-28 22:48:24 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function theme_add_bootstrap() {
 	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-wp_enqueue_style( 'style-css', get_template_directory_uri() . '/wordpressstyle.css' );
+wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
@@ -137,6 +137,22 @@ function openingtimes() {
 
 }
 add_action( 'widgets_init', 'openingtimes' );
+?>
+
+<?php
+function copyright() {
+
+	register_sidebar( array(
+		'name'          => 'Copy Right - Mobile View',
+		'id'            => 'copyright_mobile',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'copyright' );
 ?>
 
 <?php
