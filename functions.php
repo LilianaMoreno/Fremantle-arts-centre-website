1f3a0e23382e0d52c1c87ef5705900ce6a6c934a
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-05-28 18:38:51 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,9 +5,13 @@ wp_enqueue_style( 'style-css', get_template_directory_uri() . '/wordpressstyle.c
     wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
 wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.0.0', true );
+    wp_enqueue_script( 'wow', 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js', array(), 'null', false );
+
+
 }
  
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
+add_theme_support( 'post-thumbnails'); 
 
 ?>
 
@@ -50,6 +54,30 @@ add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
 ?>
 
 <?php
+	if ( get_field('newsheader') ) {
+		echo 'style="background: url(' . get_field(' newsheader') . ')"';
+	}
+?>
+
+<?php
+	if ( get_field('newspagebackground') ) {
+		echo 'style="background: url(' . get_field(' newspagebackground') . ')"';
+	}
+?>
+
+<?php
+	if ( get_field('blackrectangle') ) {
+		echo 'style="background: url(' . get_field(' blackrectangle') . ')"';
+	}
+?>
+
+<?php
+	if ( get_field('emailrectangle') ) {
+		echo 'style="background: url(' . get_field(' emailrectangle') . ')"';
+	}
+?>
+
+<?php
 function add_menu_link_class( $atts, $item, $args ) {
 if($args->link_class) {$atts['class'] = $args->link_class;
 }
@@ -62,3 +90,67 @@ if($args->list_item_class){$classes[] = $args->list_item_class;
     }
 add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
  ?>
+
+<?php
+function contactus() {
+
+	register_sidebar( array(
+		'name'          => 'Contact Us',
+		'id'            => 'contact_details',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'contactus' );
+?>
+
+<?php
+function location() {
+
+	register_sidebar( array(
+		'name'          => 'Location',
+		'id'            => 'location_details',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'location' );
+?>
+
+<?php
+function openingtimes() {
+
+	register_sidebar( array(
+		'name'          => 'Opening Times',
+		'id'            => 'opening_times',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'openingtimes' );
+?>
+
+<?php
+add_action('wp_enqueue_scripts', 'sk_wow_init_in_footer');
+
+function sk_wow_init_in_footer() {
+add_action( 'print_footer_scripts', 'wow_init' );
+ 
+}
+
+
+function wow_init() { ?>
+<script type="text/javascript">
+new WOW().init();
+ </script>    
+   <?php }
+
