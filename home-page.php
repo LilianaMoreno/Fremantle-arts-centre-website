90f7a4da9a8902bc90739acb19faae6960c2045c
LilianaMoreno <79676716+LilianaMoreno@users.noreply.github.com>
2021-05-28 22:48:33 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -33,8 +33,8 @@
           <div class="container">
               <div class="tag-grid">
                   <img width="750" class="yellow-circle img-responsive" src="<?php the_field('yellowcircle'); ?>">
-                  <h1 class="tag text-center"><?php the_field('tagline'); ?></h1>
-                  <a href="<?php the_field('taglinebutton'); ?>" class="btn btn-default btn-lg explore-music-events-button">EXPLORE MUSIC EVENTS</a>  
+                  <h1 class="tag text-center animate__animated animate__slideInRight"><?php the_field('tagline'); ?></h1>
+                  <a href="<?php the_field('taglinebutton'); ?>" class="btn btn-default btn-lg explore-music-events-button animate__animated animate__fadeIn">EXPLORE MUSIC EVENTS</a>  
               </div>
             </div>
       </section>
@@ -47,7 +47,7 @@
                 <h1 class="featured-news-and-events-title"><?php the_field('titleforfeaturednewsarticles'); ?></h1>
             </div>
              <div class="col-sm-4">
-            <a href="<?php the_field('seeallbutton'); ?>" class="see-all-button">SEE ALL <span class="fas fa-angle-right"></span> </a>
+            <a href="<?php the_field('seeallbutton'); ?>" class="see-all-button wow animate__animated animate__fadeInRight">SEE ALL <span class="fas fa-angle-right"></span> </a>
        </div>
         </div>
     <div class="row">
@@ -68,7 +68,7 @@
         <p class="Article-titles"><?php the_field('articlethreetitle'); ?></p>
          <p class="article-body"><?php the_field('articlethreedescription'); ?></p>
          <a href="<?php the_field('readmorebutton'); ?>" class="read-more-button">READ MORE <span class="fas fa-angle-right arrow"></span> </a>
-              <a href="<?php the_field('mobileseeallbutton'); ?>" class="btn btn-default btn-lg see-all-button-mobile">SEE ALL <span class="fas fa-angle-right arrow"></span> </a>  
+              <a href="<?php the_field('mobileseeallbutton'); ?>" class="btn btn-default btn-lg see-all-button-mobile wow animate__animated animate__fadeInLeft">SEE ALL <span class="fas fa-angle-right arrow"></span> </a>  
         </div>
         </div>
                     </div>
@@ -86,7 +86,7 @@
                         </div>
 <div class="col-sm-6">
     <p class="market-body"><?php the_field('marketsdescription'); ?></p>
-     <a class="btn btn-default btn-lg markets-button" href="<?php the_field('marketsbutton'); ?>">EXPLORE</a>
+     <a class="btn btn-default btn-lg markets-button wow animate__animated animate__fadeInRight" href="<?php the_field('marketsbutton'); ?>">EXPLORE</a>
               </div>
               </div>
           </div>
@@ -101,7 +101,7 @@
 <img class="about-rectangle img-responsive" width="400" src="<?php the_field('abouttitlegradient'); ?>">
                        <h1 class="index-titles about-title"><?php the_field('abouttitle'); ?></h1>
                        <p class="about-body"><?php the_field('aboutdescription'); ?></p>
-                                                 <a href="<?php the_field('aboutbutton'); ?>" class="btn btn-default btn-lg about-button"> FIND OUT MORE</a>
+                                                 <a href="<?php the_field('aboutbutton'); ?>" class="btn btn-default btn-lg about-button wow animate__animated animate__fadeInLeft"> FIND OUT MORE</a>
                     </div>
                        <div class="col-sm-6">
                            <img class="img-responsive about-image" src="<?php the_field('aboutimage'); ?>">
